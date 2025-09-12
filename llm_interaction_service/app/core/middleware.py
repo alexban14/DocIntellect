e65f9f96b70878b6c7f0c91f50418765afdba3d8
Alex Ban <alexbanut10@gmail.com>
2025-09-12 16:31:19 +0300

    feat(client|llm_interaction_service): envs adjustments and new host added to CORS

--- llm_interaction_service/app/core/middleware.py
+++ llm_interaction_service/app/core/middleware.py
@@ -14,6 +14,7 @@ def setup_cors(app: FastAPI):
         "http://localhost:8080",
         "http://localhost:8221",
         "http://localhost:8762",
+        "http://localhost:8322"
         "https://docintellect.banalexandru.online",
     ]
 
