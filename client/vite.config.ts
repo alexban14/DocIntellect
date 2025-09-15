0528a46579972da2a2af112190fba27133cb00c6
Alex Ban <alexbanut10@gmail.com>
2025-09-15 12:33:26 +0300

    feat: github actions to build docker images publish to ghcr

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['docintellect.banalexandru.online', 'localhost']
-  }
+    allowedHosts: true,
+  },
 })
